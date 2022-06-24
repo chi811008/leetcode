bd3b396f178e9d9bcb967217b92ed0883bf7058c
Seraphine Tsui <seraphine.tsui@cloudmatrix.com.tw>
2022-06-24 14:41:47 +0800

    feat: valid sudoku

--- AlgorithemI/36-Valid Sudoku.py
+++ AlgorithemI/36-Valid Sudoku.py
@@ -1,36 +1,48 @@
-board =[
-    ["5","3",".",".","7",".",".",".","."],
-    ["6",".",".","1","9","5",".",".","."],
-    [".","9","8",".",".",".",".","6","."],
-    ["8",".",".",".","6",".",".",".","3"],
-    ["4",".",".","8",".","3",".",".","1"],
-    ["7",".",".",".","2",".",".",".","6"],
-    [".","6",".",".",".",".","2","8","."],
-    [".",".",".","4","1","9",".",".","5"],
-    [".",".",".",".","8",".",".","7","9"]
-]
-# check rows
-for i in range(9): # fix position: y
-    num_box = set()
-    for j in range(9):
-        ele = board[i][j] # fix y
-        if ele == ".":
-            continue
-        if ele in num_box:
-            print(False)
-        else:
-            num_box.add(board[i][j])
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # check rows
+        square = defaultdict(set)
+        for i in range(9): # fix position: y
+            num_box = set()
+            for j in range(9):
+                ele = board[i][j] # fix y
+                if ele == ".":
+                    continue
+                if ele in num_box:
+                    return False
+                else:
+                    num_box.add(board[i][j])
+                # check 3 * 3
+                k = i // 3
+                l = j // 3
+                if ele in square[(k, l)]:
+                    return False
+                else:
+                    square[(k, l)].add(ele)
 
-# check columns
-for i in range(9): # fix position: x
-    num_box = set()
-    for j in range(9):
-        ele = board[j][i] # fix x
-        if ele == ".":
-            continue
-        if ele in num_box:
-            print(False)
-        else:
-            num_box.add(board[j][i])
+        # check columns
+        for i in range(9): # fix position: x
+            num_box = set()
+            for j in range(9):
+                ele = board[j][i] # fix x
+                if ele == ".":
+                    continue
+                if ele in num_box:
+                    return False
+                else:
+                    num_box.add(board[j][i])
+        return True
 
-# check 9 * 9
+
+# check 3 * 3
+# for i in range(9):
+#     for j in range(9):
+#         ele = board[i][j] 
+#         if ele == ".":
+#             continue
+#         k = i // 3
+#         l = j // 3
+#         if ele in square[(k, l)]:
+#             print(False)
+#         else:
+#             square[(k, l)].add(ele)
