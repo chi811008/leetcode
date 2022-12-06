f7f336a64b8414c3c76289e3ec757a902ef557c1
Seraphine_Tsui <chi811008@gmail.com>
2022-12-06 15:35:24 +0800

    feat: 211-Design Add and Search Words Data Structure

--- NeetCode/Tries/211-Design Add and Search Words Data Structure.py
+++ NeetCode/Tries/211-Design Add and Search Words Data Structure.py
@@ -20,8 +20,6 @@ class WordDictionary:
     def search(self, word: str) -> bool:
 
         def dfs(j, root):
-            curr = root
-
             for i in range(j, len(word)):
                 c = word[i]
                 if c == ".":
@@ -41,3 +39,57 @@ class WordDictionary:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+
+class TrieNode:
+    
+    def __init__(self):
+        self.children = dict()
+        self.end_of_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end_of_word = True
+            
+    def search(self, word: str) -> bool:
+        curr = self.root
+        def dfs(curr_node, i):
+            for j in range(i, len(word)):
+
+                # word[i] == "."
+                if word[j] == ".":
+                    for value in curr_node.children.values():
+                        if dfs(value, j + 1):
+                            return True
+                    return False
+                # normal condition
+                else:
+                    if word[j] not in curr_node.children:
+                        return False
+                    curr_node = curr_node.children[word[j]]
+            return curr_node.end_of_word 
+        
+        return dfs(curr, 0)
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+obj = WordDictionary()
+words = ["bad","dad","mad"]
+s = ["pad","bad",".ad","b.."]
+
+for word in words:
+    obj.addWord(word)
+
+for _ in s:
+    param_2 = obj.search(_)
+    print(param_2)
