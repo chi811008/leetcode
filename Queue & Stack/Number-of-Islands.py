035bdc3332a8eecc52166aaea43bd5d3d8cfb5e6
Seraphine TSUI 崔雅青 <seraphinetsui@gamania.com>
2024-11-17 13:40:28 +0800

    refactor: use bfs to count the number of island

--- Queue & Stack/Number-of-Islands.py
+++ Queue & Stack/Number-of-Islands.py
@@ -20,3 +20,31 @@ class Solution:
                     count += 1
 
         return count
+
+# stack and DFS
+# Time complexity: O(m*n)
+# Space complexity: O(m*n)
+class Solution2:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        stack = []
+
+        def dfs(r, c):
+            stack.append((r, c))
+            while stack:
+                r, c = stack.pop()
+                if r > rows - 1 or r < 0 or c > cols - 1 or c < 0 or grid[r][c] == "0":
+                    continue
+                grid[r][c] = "0"
+
+                stack.append((r + 1, c))
+                stack.append((r - 1, c))
+                stack.append((r, c + 1))
+                stack.append((r, c - 1))
+
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] != "0":
+                    dfs(r, c)
+                    count += 1
